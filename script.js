fb43a8717711527bd8093c82fdb85855e9ed2ae1
FaBBar13 <fab.bar@laposte.net>
2023-06-07 05:43:54 +0200

    ...aout slider...

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ let divFooter = document.querySelector("footer");
 
 let zoneJeu = document.querySelector('.zonejeu');
 
-
-
 function afficheFin() {
     divLoader.style.display = "none";
     zoneJeu.style.display = "flex";
@@ -19,3 +17,6 @@ function afficheFin() {
 }
 
 document.onload = setTimeout(afficheFin, 3700);
+
+/*  why ?  */
+let mySwiper = new Swiper ('.swiper-container');
