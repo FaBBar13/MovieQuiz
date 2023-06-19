1754023e7be87cafd14c68e4c94dde7dd62101d3
FaBBar13 <fab.bar@laposte.net>
2023-06-19 05:56:39 +0200

    modifs

--- script.js
+++ script.js
@@ -18,29 +18,21 @@ function afficheFin() {
 
 /* */
 
-let filmDejaTrouves = JSON.parse(localStorage.getItem("filmtrouve"));
-if (filmDejaTrouves === null) {
+let identDejaTrouves = JSON.parse(localStorage.getItem("identTrouve"));
+if (identDejaTrouves === null) {
     console.log('pas de local');
     let tabTrouve = [];
     // tabTrouve.push("F1");
     // tabTrouve.push("F4");
-
-    localStorage.setItem("filmtrouve", JSON.stringify(tabTrouve));
-    filmDejaTrouves = JSON.parse(localStorage.getItem("filmtrouve"));
-
-};
-console.log(filmDejaTrouves);
-
-let serieDejaTrouves = JSON.parse(localStorage.getItem("serietrouve"));
-if (serieDejaTrouves === null) {
-    console.log('pas de local');
-    let tabTrouve = [];
     // tabTrouve.push("S2");
     // tabTrouve.push("S3");
 
-    localStorage.setItem("serietrouve", JSON.stringify(tabTrouve));
-    serieDejaTrouves = JSON.parse(localStorage.getItem("serietrouve"));
+    localStorage.setItem("identTrouve", JSON.stringify(tabTrouve));
+    identDejaTrouves = JSON.parse(localStorage.getItem("identTrouve"));
+
 };
+console.log(identDejaTrouves);
+
 
 
 
@@ -54,8 +46,9 @@ let tabSerieCherche = {};
 let divSerieCherche = document.getElementById('seriecherche');
 let divSerieTrouve = document.getElementById('serietrouve');
 
-let tabFilmTrouve = {};
-let tabSerieTrouve = {};
+let tabFilmTrouve = [];
+let tabSerieTrouve = [];
+
 // stock  en memoire le fichier de base si appellé de nouveau
 let cached;
 
@@ -69,95 +62,169 @@ async function getData() {
 getData().then(data => {
 
     //console.log(data);
-    //console.log(data.movies.movie[1]);
 
-    // let tabFilmCherche = data.movies.movie;
-    // let tabSerieCherche = data.series.serie;
     tabFilmCherche = data.movies.movie;
     tabSerieCherche = data.series.serie;
 
-    //console.log('tabfilmcherche')
-    //console.log(tabFilmCherche);
+    // console.log(tabFilmCherche);
     //console.log(tabSerieCherche);
 
+    
+    // let mapFilm = new Map();
 
-    let mapFilm = new Map();
+    // tabFilmCherche.forEach(item => {
+    //     mapFilm.set(item.identifiant, item);
+    // });
+    
+    
+    
 
-    tabFilmCherche.forEach(item => {
-        mapFilm.set(item.idFilm, item);
-    });
-    //console.log(mapFilm);
+    /* tableaux des trouvés tabFilmTrouve / tabSerieTrouve */
+   // console.log(tabFilmTrouve);
+    //console.log(tabSerieTrouve);
+    for ( i =0 ; i < identDejaTrouves.length ; i++ ) {
+        let id =  identDejaTrouves[i];
+            for (x=0 ; x < tabFilmCherche.length;x++ ) {
 
-    let mapSerie = new Map();
-    tabSerieCherche.forEach(item => {
-        mapSerie.set(item.idSerie, item);
-    });
-    //console.log(mapSerie);
+                if (tabFilmCherche[x].identifiant == id) {
+                    tabFilmTrouve.push(
+                        { "identifiant" :tabFilmCherche[x].identifiant,
+                          "picture" : tabFilmCherche[x].picture ,
+                          "title" : tabFilmCherche[x].title
+                      }   
+                    );
+                };                
+                if (tabSerieCherche[x].identifiant == id) {
+                    tabSerieTrouve.push(
+                        { "identifiant" :tabSerieCherche[x].identifiant,
+                          "picture" : tabSerieCherche[x].picture ,
+                          "title" : tabSerieCherche[x].title
+                      }   
+                    );
+                };                
 
-    //console.log(objDejaTrouves);
+            }
+        
+        //console.log(id) ;
+        //console.log(tabFilmTrouve);
+        //console.log(indexTab);
+    };
 
-    function genereImages(root, tableauRoot) {
 
-        // console.log(root.id);
-        if (root.id == "filmcherche") {
+
+
+
+
+    function genereCherche(root, tableauRoot) {
+
             for (i = 0; i < tableauRoot.length; i++) {
-                if (!filmDejaTrouves.includes(tableauRoot[i].idFilm)) {
+                if (!identDejaTrouves.includes(tableauRoot[i].identifiant)) {
 
                     let divFilm = document.createElement("div");
                     let imgFilm = document.createElement("img");
-                    //let titreFilm = document.createElement("h3");
 
                     divFilm.className = "swiper-slide";
-                    divFilm.dataset.id = tableauRoot[i].idFilm;
+                    divFilm.dataset.id = tableauRoot[i].identifiant;
 
                     imgFilm.src = tableauRoot[i].picture;
 
                     let titreFilm = tableauRoot[i].title;
 
-                    //console.log(idFilm);
 
                     divFilm.appendChild(imgFilm);
-                    //divFilm.appendChild(titreFilm);
 
-                    // divFilm.addEventListener('click', e => afficheModale(divFilm));
                     divFilm.addEventListener('click', e => afficheModale(divFilm.dataset.id, imgFilm.src, titreFilm));
                     root.appendChild(divFilm);
                 }
             };
 
-        } else if (root.id == "seriecherche") {
-
-            for (i = 0; i < tableauRoot.length; i++) {
-                if (!serieDejaTrouves.includes(tableauRoot[i].idSerie)) {
-
-                    let divSerie = document.createElement("div");
-                    let imgSerie = document.createElement("img");
-                    //let idSerie = document.createElement("h3");
-
-                    divSerie.className = "swiper-slide";
-                    divSerie.dataset.id = tableauRoot[i].idSerie;
-
-                    imgSerie.src = tableauRoot[i].picture;
-
-                    let titreSerie = tableauRoot[i].title;
-
-                    //console.log(idFilm);
-
-                    divSerie.appendChild(imgSerie);
-                    //divSerie.appendChild(idSerie);
-
-                    divSerie.addEventListener('click', e => afficheModale(divSerie.dataset.id, imgSerie.src, titreSerie));
-                    root.appendChild(divSerie);
+            return  new Swiper(root.parentElement,
+                {
+                    direction: "horizontal",
+                    loop: true,
+                    grabCursor: true,
+                    centeredSlides: true,
+                    slidesPerView: 3,
+                    spaceBetween: 3,
+                    breakpoints: {
+                        // 640: {
+                        //     slidesPerView: 3
+                        // },
+                        768: {
+                            slidesPerView: 4,
+                        },
+                        992: {
+                            slidesPerView: 6,
+                        },
+                        1200: {
+                            slidesPerView: 8,
+                        },
+                    },
                 }
-            };
+            );
 
-        };
     };
+    function genereTrouve(root, tableauRoot) {
+
+        //console.log(tableauRoot[1].identifiant);
+        for (i = 0; i < tableauRoot.length; i++) {
+                let divFilm = document.createElement("div");
+                let imgFilm = document.createElement("img");
+                let titreFilm = document.createElement("h3");
+
+                divFilm.className = "swiper-slide";
+                divFilm.dataset.id = tableauRoot[i].identifiant;
+
+                imgFilm.src = tableauRoot[i].picture;
+
+                titreFilm.innerHTML = tableauRoot[i].title;
+
+                //console.log(idFilm);
+
+                divFilm.appendChild(imgFilm);
+                divFilm.appendChild(titreFilm);
+
+                //divFilm.addEventListener('click', e => afficheModale(divFilm.dataset.id, imgFilm.src, titreFilm));
+                root.appendChild(divFilm);
+            }
+            return  new Swiper(root.parentElement,
+                {
+                    direction: "horizontal",
+                    loop: true,
+                    grabCursor: true,
+                    centeredSlides: true,
+                    slidesPerView: 3,
+                    spaceBetween: 3,
+                    breakpoints: {
+                        // 640: {
+                        //     slidesPerView: 3
+                        // },
+                        768: {
+                            slidesPerView: 4,
+                        },
+                        992: {
+                            slidesPerView: 6,
+                        },
+                        1200: {
+                            slidesPerView: 8,
+                        },
+                    },
+                }
+            );
+
+
+        
+        };
 
 
 
-    genereImages(divFilmCherche, tabFilmCherche);
-    genereImages(divSerieCherche, tabSerieCherche);
+
+    genereCherche(divFilmCherche, tabFilmCherche);
+    genereCherche(divSerieCherche, tabSerieCherche);
+
+    genereTrouve(divFilmTrouve, tabFilmTrouve);
+    genereTrouve(divSerieTrouve, tabSerieTrouve);
+
 
 });
 
@@ -167,110 +234,85 @@ getData().then(data => {
 
 
 
-let reponseSaisie = document.getElementById("reponse");
+
 let reponseATrouver = "";
+let identATrouver = "";
+
+// function placeFocus() {
+//     document.getElementById("reponse").focus();
+// };
 
 function afficheModale(ident, Image, Titre) {
 
     document.getElementById("reponse").value = "";
+    
+    //placeFocus() : ? why
 
-    console.log("ds la modale");
-    console.log(ident + ' ' + Image + ' ' + Titre);
-
-    let id = ident;
     let imgSrc = Image;
-    let reponseATrouver = Titre;
-    //console.log(id + ' ' + imgSrc + ' ' + reponseATrouver);
-
-    //let item = map.get(ident);
-
-
+    identATrouver = ident;
+    reponseATrouver = Titre;
 
     let modale = document.getElementById("modale");
     let imgModal = document.getElementById("image");
-    // reponseATrouver = id; // item.title;
 
-    // let reponse = document.getElementById("reponse").value;
-    // reponse = '';
+    modale.addEventListener("submit" , verifReponse );
+
+    
     modale.style.visibility = "visible";
-
     imgModal.style.backgroundImage = `url(${imgSrc})`;
+    window.scroll({
+        top : 0
+    });
 
-
-    // Masquer la div si clique en dehors ?
+    // retour : Masquer la div si clique en dehors ?
     imgModal.addEventListener("click", function () {
         modale.style.visibility = "hidden";
     });
 
 
-
 }
 
-
-
-
-
-//genereImages(divFilmCherche, tabFilmCherche);
-//genereImages(divFilmTrouve, tabFilmTrouve);
+let libResultat = document.getElementById("resultat");
 
 
 function verifReponse() {
 
-    let reponse = document.getElementById("reponse").value;
+    let reponseSaisie = document.getElementById("reponse").value;
 
-    alert('=>' + reponse + ' pour ' + reponseATrouver);
+  
+    /* */
+    let tmpSai = "";
+    let tmpTit = "";
+    tmpSai = reponseSaisie.toLowerCase();
+    tmpSai = ((tmpSai.replaceAll('é','e')).replaceAll('è','e')).replaceAll(' ','');
+    tmpSai = tmpSai.replaceAll('à','a');
+    tmpTit = reponseATrouver.toLowerCase();
+    tmpTit = ((tmpTit.replaceAll('é','e')).replaceAll('è','e')).replaceAll(' ','');
+    tmpTit = tmpTit.replaceAll('à','a');
+    //alert('=>' + reponseSaisie +" = " + tmpSai + ' <=> ' + tmpTit + '  ' + reponseATrouver + " id = " + identATrouver);
+    
+    // libResultat.style = 'visibility : visible;'
+    
 
-    // if (reponse === )
-}
+    if ( tmpSai == tmpTit ) {
+        console.log('Bonne Réponse');
+        libResultat.style = 'color:green;'
+        libResultat.innerHTML="BONNE REPONSE";
+        let locStorage = JSON.parse(localStorage.getItem("identTrouve"));
+        locStorage.push(identATrouver);
+        localStorage.setItem("identTrouve", JSON.stringify(locStorage));
 
-
-const swiper1 = new Swiper(divFilmCherche.parentElement,
-    {
-        direction: "horizontal",
-        loop: true,
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: 3,
-        spaceBetween: 3,
-        breakpoints: {
-            // 640: {
-            //     slidesPerView: 3
-            // },
-            768: {
-                slidesPerView: 4,
-            },
-            992: {
-                slidesPerView: 6,
-            },
-            1200: {
-                slidesPerView: 8,
-            },
-        },
     }
-);
+    else {
+        console.log('Mauvaise Réponse');
+        libResultat.style = 'color:red;'
+        libResultat.innerHTML="MAUVAISE REPONSE";
+ 
+    };
+ 
+    alert('visible');
+    
+};
 
-const swiper2 = new Swiper(divSerieCherche.parentElement,
 
-    {
-        direction: "horizontal",
-        loop: true,
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: 3,
-        spaceBetween: 3,
-        breakpoints: {
-            // 640: {
-            //     slidesPerView: 3
-            // },
-            768: {
-                slidesPerView: 4,
-            },
-            992: {
-                slidesPerView: 6,
-            },
-            1200: {
-                slidesPerView: 8,
-            },
-        },
-    }
-);
+
